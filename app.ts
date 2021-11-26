4040613dbc93b5e2fe9dd1f4e1be94125e623f3c
Matduro <mathieurousseau123@gmail.com>
2021-11-25 17:13:07 -0800

    literal types

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
     let result;
 
-    if (typeof input1 === "number" && typeof input2 === "number") {
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
         result = input1 + input2
     } else {
         result = input1.toString() + input2.toString()
@@ -9,5 +9,5 @@ function combine(input1: number | string, input2: number | string) {
     return result
 }
 
-const combinedAges = combine(30, 26)
+const combinedAges = combine(30, 26, "as-number")
 console.log(combinedAges);

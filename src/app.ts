f6e28f39c0bd20b3a6a395860b329b07d0e623a8
Matduro <mathieurousseau123@gmail.com>
2021-12-03 13:02:42 -0800

    generics and constraints

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,36 @@
-const names = ["Max", "Emanuelle"]
+// const names = ["Max", "Emanuelle"]
+const names: Array<string> = []
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("This is done!")
+    }, 2000)
+})
+
+promise.then(data => data.split(" "))
+
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+function mergeWithContraints<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergedObj = merge({name: 'Mac', hobbies: ['Soccer']}, {age: 30})
+console.log(mergedObj.age);
+
+interface Lengthy {
+    length: number
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = "Got no value."
+    if (element.length === 1 ) {
+        descriptionText = 'Got 1 element.'
+    } else if (element.length > 1) {
+        descriptionText = 'Got' + element.length + ' elements.'
+    }
+    return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hello there amigo!'));
